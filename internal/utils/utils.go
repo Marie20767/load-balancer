82a6958f0dc7881f7d64e2794a9af2a47914e2b6
Marie20767 <marie20767@gmail.com>
2025-08-18 15:36:25 +0200

    Implemented custom error handler and loadbalancer

---
+++ internal/utils/utils.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/labstack/echo/v4"
+)
+
+func CustomErrHandler (err error, c echo.Context) {
+	if c.Response().Committed {
+			return
+	}
+
+	var code int
+	var msg string
+
+	if _, isURLError := err.(*url.Error); isURLError {
+			code = http.StatusBadGateway
+			msg = "Bad Gateway: " + err.Error()
+	} else {
+			code = http.StatusInternalServerError
+			msg = err.Error()
+	}
+
+	c.String(code, msg)
+}
--- main.go
+++ main.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
+	"net/http/httputil"
+	"net/url"
 	"os"
 
+	"github.com/Marie20767/load-balancer/internal/utils"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
-func loadBalancer(c echo.Context) error {
-	// TODO: route to one of the servers with round robin or first without
+type Spec struct {
+	Url     string
+	S1_port string
+	S2_port string
+	S3_port string
+}
+
+func loadBalancer(spec *Spec) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		req := c.Request()
+
+		target := fmt.Sprintf("%s:%s", spec.Url, spec.S1_port)
+		targetURL, err := url.Parse(target)
+		if err != nil {
+			return err
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(targetURL)
+		req.Header.Set("X-Forwarded-For", c.RealIP())
+		proxy.ServeHTTP(c.Response(), req)
+
+		return nil
+	}
 }
 
 func run() error {
@@ -19,12 +44,25 @@ func run() error {
 	}
 
 	port := os.Getenv("PORT")
-	if port == "" {
+	URL := os.Getenv("SERVER_URL")
+	s1_port := os.Getenv("SERVER_1_PORT")
+	s2_port := os.Getenv("SERVER_2_PORT")
+	s3_port := os.Getenv("SERVER_3_PORT")
+
+	spec := &Spec{
+		Url:     URL,
+		S1_port: s1_port,
+		S2_port: s2_port,
+		S3_port: s3_port,
+	}
+
+	if spec.Url == "" || spec.S1_port == "" || spec.S2_port == "" || spec.S3_port == "" || port == "" {
 		return errors.New("not all environment variables are set")
 	}
 
 	e := echo.New()
-	e.GET("*", loadBalancer)
+	e.HTTPErrorHandler = utils.CustomErrHandler
+	e.Any("/*", loadBalancer(spec))
 
 	return e.Start(":" + port)
 }
